a99ed1a77290a2367773711b73e1416e6d65b03e
moisesr <moisesr@spotify.com>
2025-04-12 20:25:46 +0200

    Attemtp to get dates from filenames and new extension

--- organize-multimedia.linux-unix.sh
+++ organize-multimedia.linux-unix.sh
@@ -29,8 +29,18 @@ then
     echo "identify command not found!"
     exit 3
 fi
+
+which ffprobe > /dev/null
+# make sure ffprobe command is installed
+if [ $? -eq 1 ]
+then
+    echo "ffprobe command not found!"
+    exit 3
+fi
+
+
 # using a tmp file you can have spaces in the file path
-find "$cfgSource" -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.nef" -o -iname "*.png" -o -iname "*.bmp" -o -iname "*.avi" -o -iname "*.flv" -o -iname "*.VOB" -o -iname "*.mov" -o -iname "*.mpg" -o -iname "*.mp4" > images.tmp
+find "$cfgSource" -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.nef" -o -iname "*.png" -o -iname "*.bmp" -o -iname "*.avi" -o -iname "*.flv" -o -iname "*.VOB" -o -iname "*.mov" -o -iname "*.mpg" -o -iname "*.mp4" -o -iname "*.gif" > images.tmp
 
 cat ./images.tmp | while read f;
 do
@@ -40,14 +50,62 @@ do
                 echo " "
                 FILETYPE=$(toUpper ${f#*.})
                 timestamp=""
-                if [ "$FILETYPE" = "JPG" -o "$FILETYPE" = "NEF" ]; then
-                    timestamp="$(identify -format '%[exif:DateTimeOriginal]' ${f})"
+                if [ "$FILETYPE" = "JPG" -o "$FILETYPE" = "NEF" -o "$FILETYPE" = "PNG" -o "$FILETYPE" = "JPEG" -o "$FILETYPE" = "BMP" ]; then
+
+                    EXIF_TAGS=(DateTimeOriginal DateTimeDigitized DateTime)
+                    for TAG in "${EXIF_TAGS[@]}"
+                    do
+                      timestamp="$(identify -format "%[exif:${TAG}]" "${f}")"
+                    
+                      if [ ! "${timestamp}" = "" ]; then
+                          break
+                      fi
+
+                    done
+                  
                     timestamp=${timestamp%T*}
+                elif [ "$FILETYPE" = "MOV" -o "$FILETYPE" = "MP4" -o "$FILETYPE" = "FLV" -o "$FILETYPE" = "MPG" -o "$FILETYPE" = "VOB" -o "$FILETYPE" = "AVI" ]; then
+                    # Video files needs a different way to check their metadata  
+                    timestamp="$(ffprobe -v quiet "${f}" -show_entries stream=index,codec_type:stream_tags=creation_time:format_tags=creation_time |  grep -Eo '20([0-9]{2}-){2}[0-9]{2}' | head -1)"
                 fi
+
                 #If identify fails to read the date from exif
-                # or file is not an image, use stat to get last modification date
+                # or file is not an image, try to get date from filename
                 if [ "${timestamp}" = "" ]; then
-                    timestamp=$(stat -c %y "$f")
+
+                    # All together in the file name 
+                    # Ex: 20110101
+                    # also adding dashes so it will end up as
+                    #   2011-01-01
+                    timestamp=$(echo "$f" | grep -Eo '20[0-9]{6}' | sed 's/\(....\)\(..\)/\1-\2-/')
+                    
+                    # Double checking if possible date found is not in the future
+                    if [ ! "${timestamp}" = "" ]; then
+                        yearFound=$(echo $timestamp | cut -c 1-4)
+
+                        currentYear=$(date '+%Y')
+                        if [ $yearFound > $currentYear ]; then
+                            timestamp=""
+                        fi
+                    fi
+
+                fi
+                if [ "${timestamp}" = "" ]; then
+
+                    # Dash separated datee
+                    # Ex: 2020-01-02
+                    timestamp=$(echo "$f" | grep -Eo '20([0-9]{2}-){2}[0-9]{2}')
+
+                fi
+
+                # Last resort, get date from last modified file metadata
+                if [ "${timestamp}" = "" ]; then
+
+                    if [[ $OSTYPE == 'darwin'* ]]; then
+                      timestamp=$(stat -f %SB -t %Y-%m-%d "$f")
+                    else
+                      timestamp=$(stat -c %y "$f")
+                    fi
                 fi
                 # Looks like there are three possible timestamp formats:
                 #       2014-05-05T14:46:47.16+01:00
@@ -57,10 +115,12 @@ do
                 y=$(echo $timestamp | cut -c 1-4)
                 m=$(echo $timestamp | cut -c 6-7)
                 d=$(echo $timestamp | cut -c 9-10)
-                destFile=$cfgDestRoot/$y/$m/$d/$(basename "${f}")
+                #dateBaseDir=$y/$m/$d
+                dateBaseDir=$y/$m
+                destFile=$cfgDestRoot/$dateBaseDir/$(basename "${f}")
                 # If the directory doesn't exist recursively create it.
-                if [ ! -d "$cfgDestRoot/$y/$m/$d" ]; then
-                        mkdir -p "$cfgDestRoot/$y/$m/$d"
+                if [ ! -d "$cfgDestRoot/$dateBaseDir" ]; then
+                        mkdir -p "$cfgDestRoot/$dateBaseDir"
                 fi
                 # Move the file.
                 if [ -f "${destFile}" ]; then
@@ -82,8 +142,10 @@ do
                                 echo "Duplicate Found, keeping the larger file."
                                 if [ $sizeSrc -gt $sizeDst ]; then
                                         mv "$f" "$destFile"
+                                        #echo "mv \"$f\" \"$destFile\""
                                 else
                                         rm "$f"
+                                        #echo "rm \"$f\""
                                 fi
                         fi
                 else
@@ -103,4 +165,4 @@ find "$cfgSource" -iname "*.IND" -exec rm -v {} +
 find "$cfgSource" -iname "Thumbs.db" -exec rm -v {} +
 find "$cfgSource" -type d -empty -delete
 #Delete temporary file
-rm images.tmp
+#rm images.tmp
